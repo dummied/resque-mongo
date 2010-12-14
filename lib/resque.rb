bd6b5ca261b11417d5535a6a4068143c3d648b8f
Nathan Acuff <naginata@gmail.com>
2010-12-14 18:14:39 -0500

    roll back _id sort change, it's just not fast enough

--- lib/resque.rb
+++ lib/resque.rb
@@ -182,7 +182,8 @@ module Resque
       query['delay_until'] = { '$not' => { '$gt' => Time.new}}
     end
     #sorting will result in significant performance penalties for large queues, you have been warned.
-    item = mongo[queue].find_and_modify(:query => query, :remove => true, :sort => [[:_id, :asc]] )
+    #this applies to _id, too.  it'd be nice, but it won't work, so don't try it.
+    item = mongo[queue].find_and_modify(:query => query, :remove => true) #, :sort => [[:_id, :asc]] )
   rescue Mongo::OperationFailure => e
     return nil if e.message =~ /No matching object/
     raise e
